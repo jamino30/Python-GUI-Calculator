cc04c5ff9811ad5fba4c1da8a6cde8482dade7ce
Jai Amin <79227536+jamino30@users.noreply.github.com>
2022-11-30 17:00:02 -0500

    Update format

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-"""A basic calculator app with custom GUI display:- Unary Operations (+/-, %)- Binary Operations (+, -, *, /)- Keyboard inputsNote: Utilizes eval() safely and prevents userfrom issuing dangerous commands/functions.MIT LicenseCopyright (c) 2022 Jai Amin"""from gui import CalculatorApp, tkif __name__ == "__main__":    root = tk.Tk()    # configure constant window sizing    width = 230    height = 300    cen_width = int(root.winfo_screenwidth() / 2 - (width / 2))    cen_height = int(root.winfo_screenheight() / 2 - (height / 2))    root.geometry(f"{width}x{height}+{cen_width}+{cen_height}")    root.resizable(False, False)    # misc. window attributes    root.wm_title("Calculator")    icon = tk.PhotoImage(file='~/Pictures/logo.png')    root.wm_iconphoto(False, icon)    CalculatorApp(root)    root.mainloop()
+"""
+Basic calculator app with custom GUI display:
+- Unary Operations (+/-, %)
+- Binary Operations (+, -, *, /)
+- Keyboard inputs
+
+Note: Utilizes eval() safely and prevents user
+from issuing dangerous commands/functions.
+
+MIT License
+Copyright (c) 2022 Jai Amin
+"""
+
+from gui import CalculatorApp, tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+
+    # configure constant window sizing
+    width = 230
+    height = 300
+    cen_width = int(root.winfo_screenwidth() / 2 - (width / 2))
+    cen_height = int(root.winfo_screenheight() / 2 - (height / 2))
+    root.geometry(f"{width}x{height}+{cen_width}+{cen_height}")
+    root.resizable(False, False)
+
+    # misc. window attributes
+    root.wm_title("Calculator")
+    icon = tk.PhotoImage(file='~/Pictures/logo.png')
+    root.wm_iconphoto(False, icon)
+
+    CalculatorApp(root)
+    root.mainloop()
